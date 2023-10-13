aa36ede27a149315fe77b8ad762e6c365122ed22
CrisUrrea <cristhianca2009@gmail.com>
2023-10-13 15:06:45 -0500

    Creacion plantilla y estilos

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 
-    const btn = document.getElementById('button');
+const btn = document.getElementById('button');
 
 document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
 
-   btn.value = 'Sending...';
+    btn.value = 'Sending...';
 
-   const serviceID = 'default_service';
-   const templateID = 'template_btoj7p3';
+    const serviceID = 'default_service';
+    const templateID = 'template_btoj7p3';
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      Swal.fire(
-        'Bien hecho!',
-        'Correo enviado con exito!',
-        'success'
-      )
-      form.reset();
-    }, (err) => {
-      btn.value = 'Send Email';
-      Swal.fire(
-        'Error!',
-        JSON.stringify(err),
-        'error'
-      )
-    });
-});
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        Swal.fire(
+          'Bien hecho!',
+          'Correo enviado con exito!',
+          'success'
+        )
+        form.reset();
+      }, (err) => {
+        btn.value = 'Send Email';
+        Swal.fire(
+          'Error!',
+          JSON.stringify(err),
+          'error'
+        )
+      });
+  });
--- pageppl.js
+++ pageppl.js
@@ -2,12 +2,12 @@
 window.addEventListener("load", function () {
     const btnbuilding = document.querySelectorAll('.building');
     btnbuilding.forEach(element => {
-        element.addEventListener('click',function (e) {
+        element.addEventListener('click', function (e) {
             Swal.fire(
                 'Lo sentimos!',
                 'La sección a la que intentas acceder aún esta en construcción!',
                 'warning'
-              )
+            )
         })
     });
 })
